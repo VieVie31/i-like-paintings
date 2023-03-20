8f695415d4308a4c4dabfdddd8468c79dbc792d5
NicolasBizzozzero <nicolas.bizzozzero@protonmail.com>
2023-03-20 12:32:29 +0100

    Added long_description for PyPI

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 
 setup(
     name='ilikepaintings',
@@ -7,6 +12,8 @@ setup(
     author='Nicolas BIZZOZZERO, Ihab BENDIDI, Olivier RISSER-MAROIX',
     author_email='orissermaroix@gmail.com',
     description='A package for predicting painting appreciation from images using a linear regressor on top of a frozen CLIP model',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/VieVie31/i-like-painings',
     packages=find_packages(),
     install_requires=[
